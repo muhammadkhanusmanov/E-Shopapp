70d276830141ae09917e09e4eb90905386f6f66d
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-11-25 20:20:53 +0500

    sign up

--- main/views.py
+++ main/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from django.http import HttpRequest,JsonResponse,FileResponse
+from rest_framework.parsers import MultiPartParser, FormParser
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+from rest_framework.permissions import IsAuthenticated,IsAdminUser
+from rest_framework.authentication import TokenAuthentication, BasicAuthentication
 
-# Create your views here.
+
+
+class UserView(APIView):
+    '''Sign up a user'''
+    def post(self, request):
+        data = request.data
+        username = data.get('username')
+        password = data.get('password')
+        number = data.get('number')
+        try:
+            user = User.objects.get(first_name=number)
+            return Response({'status':'This number is already'},status=status.HTTP_208_ALREADY_REPORTED)
+        except:
+            try:
+                user = User.objects.create(
+                    username = username,
+                    password = make_password(password),
+                    first_name = number
+                )
+                token = Token.objects.create(user=user)
+                return Response({'status':'Create a user', 'token':token.key},status=status.HTTP_201_CREATED)
+            except:
+                return Response({'status':'This username is already'},status=status.HTTP_208_ALREADY_REPORTED)
+
