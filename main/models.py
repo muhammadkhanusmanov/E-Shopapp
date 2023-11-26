c2851f4f4736f8041298135d45d6bed71f085716
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-11-26 10:18:28 +0500

    category model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    photo = models.ImageField(upload_to='categoryimge')
