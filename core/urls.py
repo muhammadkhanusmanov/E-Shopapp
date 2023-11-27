f8385a76fe176cb0605dc48e91e9a6401c4e63c6
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-11-27 21:19:37 +0500

    v3

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/',include('main.urls.userurls')),
-    path('api/v2/',include('main.urls.categoryurls'))
+    path('api/v2/',include('main.urls.categoryurls')),
+    path('api/v3/',include('main.urls.producturl'))
 ]
