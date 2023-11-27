8224065d3ce1a8081081754e752b9dc8ea54e5bf
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-11-27 20:58:17 +0500

    .

--- main/serializers/serializers.py
+++ main/serializers/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework.serializers import ModelSerializer
-from ..models import Categories
+from ..models import Categories, Products
 
 class CategorySerializer(ModelSerializer):
     class Meta:
         model = Categories
+        fields = '__all__'
+
+class ProductsSerialzer(ModelSerializer):
+    category = CategorySerializer(many=True)
+    class Meta:
+        model = Products
         fields = '__all__'
