ad7d36de96f4ab4b16d70b641ee188a09a247680
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-11-27 11:00:08 +0500

    url

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/',include('main.urls.userurls'))
+    path('api/v1/',include('main.urls.userurls')),
+    path('api/v2/',include('main.urls.categoryurls'))
 ]
