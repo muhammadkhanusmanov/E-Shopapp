ec2330725b085dc6f9dc443bc730c3cf68dc35d0
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-11-29 14:28:48 +0500

    init

--- main/serializers/serializers.py
+++ main/serializers/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework.serializers import ModelSerializer
 from ..models import Categories, Products
+from rest_framework import serializers
 
 class CategorySerializer(ModelSerializer):
     class Meta:
@@ -7,6 +8,10 @@ class CategorySerializer(ModelSerializer):
         fields = '__all__'
 
 class ProductsSerialzer(ModelSerializer):
+    category = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='name'
+     )
     class Meta:
         model = Products
         fields = '__all__'
