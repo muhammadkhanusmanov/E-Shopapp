8e4536e58fdc90c69390f6ae45fcb153f65b76c8
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-11-30 10:03:16 +0500

    url

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/',include('main.urls.userurls')),
     path('api/v2/',include('main.urls.categoryurls')),
-    path('api/v3/',include('main.urls.producturl'))
+    path('api/v3/',include('main.urls.producturl')),
+    path('api/v4/',include('main.urls.buying')),
 ]
