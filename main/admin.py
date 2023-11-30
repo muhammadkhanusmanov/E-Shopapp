9fac56cb2dca400dafe8c5ae71f958601a6a3236
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-11-30 10:01:44 +0500

    init

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Categories, Products
+from .models import Categories, Products, UserProduct
 
 admin.site.register([
-    Categories,Products
+    Categories,Products, UserProduct
 ])
