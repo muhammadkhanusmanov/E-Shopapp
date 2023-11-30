cfa3589f01172c67b85845dc3d7a7fb40d1ff459
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-11-30 11:27:08 +0500

    url

--- main/urls/producturl.py
+++ main/urls/producturl.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from ..views import (
-    ProductView
+    ProductView, SearchProduct
 )
 
 
 urlpatterns = [
     path('allproducts/',ProductView.as_view()),
-    path('productbyid/<str:id>',ProductView.as_view())
+    path('productbyid/<str:id>',ProductView.as_view()),
+    path('search/product/<str:name>',SearchProduct.as_view())
 ]
