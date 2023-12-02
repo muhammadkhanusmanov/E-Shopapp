49061ce07904ef4913839eacabb8cc3002a0fd9e
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-12-02 21:08:39 +0500

    enpoint reklama

--- main/urls/producturl.py
+++ main/urls/producturl.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 from ..views import (
-    ProductView, SearchProduct
+    ProductView, SearchProduct,Reklama
 )
 
 
 urlpatterns = [
     path('allproducts/',ProductView.as_view()),
     path('productbyid/<str:id>',ProductView.as_view()),
-    path('search/product/<str:name>',SearchProduct.as_view())
+    path('search/product/<str:name>',SearchProduct.as_view()),
+    path('special/',Reklama.as_view()),
 ]
