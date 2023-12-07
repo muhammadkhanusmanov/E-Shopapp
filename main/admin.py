3a8574e39924e3d85fd3efcab1d6ba84efdfaf7e
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-12-07 14:30:56 +0500

    register

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Categories, Products, UserProduct, MarketProduct, RecomntsProduct
+from .models import Categories, Products, UsersProduct, MarketProduct, RecomntsProduct
 
 admin.site.register([
-    Categories,Products, UserProduct, MarketProduct,RecomntsProduct
+    Categories,Products, UsersProduct, MarketProduct,RecomntsProduct
 ])
